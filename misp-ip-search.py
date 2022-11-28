835c1dcd39942dec85e219200ca351ebe157ef17
gomerBGU <omergo66@gmail.com>
2022-11-28 10:31:28 +0200

    update limit.txt and ini file

--- misp-ip-search.py
+++ misp-ip-search.py
@@ -1,3 +1,4 @@
+import configparser
 import ipaddress
 import os
 import re
@@ -7,10 +8,22 @@ import time
 import csv
 import argparse
 
-
-misp_url = 'https://misppriv.circl.lu'
-misp_key = 'Ev0eihJb8VasYZXTLy5Oc5uhHesUIv9CKqxmAN3f'
-misp_verifycert = False
+# Read the config file
+config = configparser.ConfigParser()
+try:
+    config.read('./misp-ip-search.ini')
+    # MISP config
+    has_MISP = False
+    if config.has_section('MISP'):
+        MISP_URL = config.get('MISP', 'MISP_URL').replace('\'','')
+        MISP_AUTH_KEY = config.get('MISP', 'MISP_AUTH_KEY').replace('\'','')
+    if MISP_URL == '' or MISP_AUTH_KEY == '':
+        print("[E] Please provide the url and the api key in 'misp-ip-search.ini'")
+        exit(1)
+except Exception as e:
+    print("[E] Config file 'misp-ip-search.ini' not found or missing field - check the current misp-ip-search.ini template if fields have "
+            "changed or add the missing field manually")
+    exit(1)
 
 def is_ip(value):
     ip_pattern = r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b$'
@@ -24,7 +37,7 @@ def is_private(ip):
 
 def make_link_to_event(eventid):
     # TODO: Make sure that there's a trailing slash on the MISP_SERVER param
-    return("<" + misp_url + "events/view/" + str(eventid) + "|Event " + str(eventid) + ">")
+    return("<" + MISP_URL + "events/view/" + str(eventid) + "|Event " + str(eventid) + ">")
 
 def make_timestamp(ts):
     return("<!date^" + str(int(ts.timestamp())) + "^{date_num} {time}|timestamp>" )
@@ -65,11 +78,15 @@ def misp_search_ip(ip):
     update_limit()
     requests.packages.urllib3.disable_warnings()  # I don't care
     attribute_types = ["ip-src","ip-dst"]
-    misp = pymisp.PyMISP(misp_url, misp_key, False, False, proxies={},cert=None,auth=None,tool='Cyght : Online ip checker')
+    misp = pymisp.PyMISP(MISP_URL, MISP_AUTH_KEY, False, False, proxies={},cert=None,auth=None,tool='Cyght : Online ip checker')
     result = misp.search(value=ip, type_attribute= ["ip-src","ip-dst"], limit=11, published=True, to_ids=True, deleted=False, pythonify=True)          
     return(render_results(result, ip, attribute_types))
 
 def read_limit():
+    if not os.path.exists('./limit.txt'):
+        limit_file = open('./limit.txt', "w")
+        limit_file.write('0')
+        limit_file.close
     limit_file = open('./limit.txt', "r")
     limit = int(limit_file.readline().strip())
     limit_file.close()
@@ -99,17 +116,21 @@ args = parser.parse_args()
 # Check args
 if args.f != '' and args.i !='':
     print("[E] Please provide only an input file with '-f' input file or a single ip address with '-i' ip address\n")
+    input('Press any key to exit\n')
     parser.print_help()
     exit(1)
 if args.f == '' and args.i =='':
     print("[E] Please provide an input file with '-f' input file or a single ip address with '-i' ip address\n")
+    input('Press any key to exit\n')
     parser.print_help()
     exit(1)
 if args.f != '' and not os.path.exists(args.f):
     print("[E] Cannot find input file {0}".format(args.f) + ", please Enter the full path")
+    input('Press any key to exit\n')
     exit(1)
 if args.f != '' and not args.f.__contains__('.txt'):
     print("[E] Please provide a txt File")
+    input('Press any key to exit\n')
     exit(1)
 
 # Scan input file
@@ -145,16 +166,19 @@ if args.f != '':
             print('Requests sent (for valid and non-private addresses) in the current limit: ' +str(read_limit())+'\n')
     f.close()
     print('End of Scan..')
+    input('Press any key to exit')
     exit(0)
 
 # Case that the single IP address is not a valid IP address
 if not is_ip(args.i):
     print("[E] Please a valid IP address")
+    input('Press any key to exit\n')
     exit(1)
 
 # Case that the single IP address is private
 if not is_private(args.i):
     print(args.i + 'is a private address.')
+    input('Press any key to exit\n')
     exit(0)
 # Check only a single IP address
 if read_limit() == 15:
@@ -163,3 +187,5 @@ if read_limit() == 15:
     print('Wake up..')
 info = misp_search_ip(args.i)
 print(info)
+input('Press any key to exit\n')
+
